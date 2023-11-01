fdd080efe9afccaaba4b1f367b9b02c87dceb843
tigran222 <tigransargsyan222@gmail.com>
2023-11-01 10:54:01 +0400

    Second commit

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "jenkins_master" {
-  ami                    = "ami-06dd92ecc74fdfb36"                # Replace with your desired AMI
-  instance_type          = "t2.micro"                             # Adjust the instance type as needed
-  subnet_id              = aws_subnet.public[0].id                # Choose one of the public subnets
-  vpc_security_group_ids = [aws_security_group.jenkins_master.id] # Associate with the security group
-  key_name               = "devops"
+  ami           = "ami-06dd92ecc74fdfb36" # Replace with your desired AMI
+  instance_type = "t2.micro"              # Adjust the instance type as needed
+  # subnet_id              = aws_subnet.public[0].id                # Choose one of the public subnets
+  # vpc_security_group_ids = [aws_security_group.jenkins_master.id] # Associate with the security group
+  key_name = "devops"
   tags = {
     Name = "Jenkins-Server"
   }
